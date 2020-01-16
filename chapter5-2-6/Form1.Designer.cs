ed480db5725209d7eb6e3de7b3cb5e23a886d227
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-01-16 21:12:14 +0800

    增加 mdb 与 accdb 数据库连接方式的对比

--- chapter5-2-6/Form1.Designer.cs
+++ chapter5-2-6/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btn1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn1
@@ -37,15 +38,26 @@
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(89, 44);
             this.btn1.TabIndex = 0;
-            this.btn1.Text = "测试";
+            this.btn1.Text = "测试mdb";
             this.btn1.UseVisualStyleBackColor = true;
             this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "测试accdb";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(546, 209);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btn1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- chapter5-2-6/Form1.cs
+++ chapter5-2-6/Form1.cs
@@ -10,7 +10,7 @@ namespace chapter5_2_6 {
             InitializeComponent();
         }
 
-        private readonly OleDbConnection oleDb = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\Code\C#_code\RiderLearning\GoatVS\chapter5-2-6\myAccess.mdb;Persist Security Info=False");
+        private readonly OleDbConnection oleDb = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\Code\C#_code\RiderLearning\GoatVS\chapter5-2-6\db\myAccess.mdb;Persist Security Info=False");
 
         private void btn1_Click(object sender, EventArgs e){
             oleDb.Open();
@@ -18,6 +18,18 @@ namespace chapter5_2_6 {
             var dbDataAdapter = new OleDbDataAdapter(sql, oleDb); //创建适配对象
             var dt = new DataTable(); //新建表对象
             dbDataAdapter.Fill(dt); //用适配对象填充表对象
+        }
+
+
+        private readonly OleDbConnection oleDb2 = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\Code\C#_code\RiderLearning\GoatVS\chapter5-2-6\db\shit.accdb;Persist Security Info=False");
+// 如果使用这种方式报错：    private readonly OleDbConnection oleDb2 = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\Code\C#_code\RiderLearning\GoatVS\chapter5-2-6\db\shit.accdb;Persist Security Info=False"); // 不可识别的数据库格式
+        private void button1_Click(object sender, EventArgs e) {
+            oleDb2.Open();
+            const string sql = "select * from sys_user";
+            var dbDataAdapter = new OleDbDataAdapter(sql, oleDb2); //创建适配对象
+            var dt = new DataTable(); //新建表对象
+            dbDataAdapter.Fill(dt); //用适配对象填充表对象
+
         }
     }
 }
