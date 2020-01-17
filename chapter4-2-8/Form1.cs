eb0ed297a28d4e3ff580ebdf301d82de3b41a0de
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-01-17 15:47:15 +0800

    完善   chapter4-2-8  spring.net + DB

--- chapter4-2-8/Form1.cs
+++ chapter4-2-8/Form1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Data;
 using System.Windows.Forms;
+using chapter4_2_8.IBLL;
+using Spring.Context;
+using Spring.Context.Support;
 
 namespace chapter4_2_8 {
     public partial class Form1 : Form {
@@ -8,9 +12,19 @@ namespace chapter4_2_8 {
         }
 
         private void btnSqlserver_Click(object sender, EventArgs e) {
+
+            // 从配置文件读取配置
+            IApplicationContext ctx = ContextRegistry.GetContext();
+            // 获取具体的实现类
+            IDatabaseService dbService = ctx.GetObject("bll") as IDatabaseService;
+            // 从数据库查询数据
+            DataTable dt = dbService.GetDataTableBySQL();
+            // 将查询出的数据绑定到DataGridView中
+
         }
 
         private void btnOracle_Click(object sender, EventArgs e) {
+
         }
     }
 }
