4e99b2ae29ef356917d17a11caa84ab2c652411d
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-01-21 23:05:12 +0800

    添加 chapter1-9-1  I/O

---
+++ chapter1-9-1/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace chapter1_9_1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            using (StreamWriter sw = new StreamWriter(@"F:\YYYest.txt",true,Encoding.UTF8)) {
+                sw.WriteLine(DateTime.Now);
+                sw.WriteLine("");
+                sw.Close();
+            }
+        }
+    }
+}
