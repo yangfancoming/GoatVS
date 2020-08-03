4e3d13a83ce19406c6a0aae582f015287286ecc3
yangfan <34465021+jwfl724168@users.noreply.github.com>
2020-08-03 09:36:27 +0800

    代码格式优化

--- chapter1-9-8/Model.cs
+++ chapter1-9-8/Model.cs
@@ -1,8 +1,5 @@
 namespace chapter2_0_3 {
     public class Model {
-//        public public sign;
-//        public public timestamp;
-//        public public body;
         public string appid;
         public string format;
         public string biztype;
